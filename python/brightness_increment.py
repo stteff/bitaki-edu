ac14cedce6c31ef985b7135d81d8da96a60690e3
Stefanos Tsitsimpasis <39708387+stteff@users.noreply.github.com>
2019-10-20 21:52:44 +0300

    Update brightness_increment.py

--- python/brightness_increment.py
+++ python/brightness_increment.py
@@ -4,11 +4,11 @@ bright = 0
 pin0.write_analog(bright)
 while True:
 
-    if button_a.is_pressed():
+    if button_a.was_pressed():
         bright = bright + 100
         pin0.write_analog(bright)
             
-    if button_b.is_pressed():
+    if button_b.was_pressed():
         bright = bright - 100
         pin0.write_analog(bright)
 
